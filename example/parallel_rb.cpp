aa6ae83c987db1063e5e389a3bc60ca00181a77e
felipehidequel <felipehidequel@gmail.com>
2023-12-13 23:39:12 -0300

    parallel zone optimization

--- example/parallel_rb.cpp
+++ example/parallel_rb.cpp
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include <math.h>
+#include <omp.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <omp.h>
+
 #include "Autotuning.hpp"
 
 #define Tolerance 0.000012
@@ -14,12 +15,9 @@
 
 double **A, **B, diff;
 
-void display(double **V, int n)
-{
-  for (int i = 0; i < n + 1; ++i)
-  {
-    for (int k = 0; k < n + 1; ++k)
-    {
+void display(double **V, int n) {
+  for (int i = 0; i < n + 1; ++i) {
+    for (int k = 0; k < n + 1; ++k) {
       printf("%f ", V[i][k]);
     }
     printf("\n");
@@ -27,75 +25,45 @@ void display(double **V, int n)
   printf("\n");
 }
 
-void initialize(double **A, int n)
-{
+void initialize(double **A, int n) {
   int i, j;
 
-  for (j = 0; j < n + 1; j++)
-  {
+  for (j = 0; j < n + 1; j++) {
     A[0][j] = 1.0;
   }
-  for (i = 1; i < n + 1; i++)
-  {
+  for (i = 1; i < n + 1; i++) {
     A[i][0] = 1.0;
-    for (j = 1; j < n + 1; j++)
-      A[i][j] = 0.0;
+    for (j = 1; j < n + 1; j++) A[i][j] = 0.0;
   }
 }
 
-long usecs(void)
-{
+long usecs(void) {
   struct timeval t;
   gettimeofday(&t, NULL);
   return t.tv_sec * 1000000 + t.tv_usec;
 }
 
-double matrix_calculation(double **A, int n, int *chunk)
-{
+double matrix_calculation(double **A, int n, int *chunk) {
   double tmp;
   double diff;
   int i, j;
 
-#pragma omp parallel num_threads(THREAD_COUNT) shared(chunk) private(tmp, i, j) reduction(+ : diff)
-  {
-#pragma omp for collapse(2) schedule(dynamic, *chunk)
-    for (i = 1; i <= n; ++i)
-    {
-      for (j = 1; j <= n; ++j)
-      {
-        if ((i + j) % 2 == 1)
-        {
-          tmp = A[i][j];
-          A[i][j] = 0.2 * (A[i][j] + A[i][j - 1] + A[i - 1][j] + A[i][j + 1] + A[i + 1][j]);
-          diff += fabs(A[i][j] - tmp);
-        }
-      }
+#pragma omp parallel for collapse(2) shared(chunk) private(tmp, i, j) reduction(+:diff) \
+    schedule(dynamic, *chunk)
+  for (i = 1; i <= n; ++i) {
+    for (j = 1; j <= n; ++j) {
+      tmp = A[i][j];
+      if ((i + j) % 2 == 1)
+        A[i][j] = 0.2 * (A[i][j] + A[i][j - 1] + A[i - 1][j] + A[i][j + 1] + A[i + 1][j]);
+      else if ((i + j) % 2 == 0)
+        A[i][j] = 0.2 * (A[i][j] + A[i][j - 1] + A[i - 1][j] + A[i][j + 1] + A[i + 1][j]);
+      diff += fabs(A[i][j] - tmp);
     }
-#pragma omp barrier
-  // }
-
-// #pragma omp parallel num_threads(THREAD_COUNT) shared(chunk) private(tmp, i, j) reduction(+ : diff)
-  // {
-#pragma omp for collapse(2) schedule(dynamic, *chunk)
-    for (i = 1; i <= n; ++i)
-    {
-      for (j = 1; j <= n; ++j)
-      {
-        if ((i + j) % 2 == 0)
-        {
-          tmp = A[i][j];
-          A[i][j] = 0.2 * (A[i][j] + A[i][j - 1] + A[i - 1][j] + A[i][j + 1] + A[i + 1][j]);
-          diff += fabs(A[i][j] - tmp);
-        }
-      }
-    }
-#pragma omp barrier
   }
   return diff;
 }
 
-void solve_parallel(double **A, int n)
-{
+void solve_parallel(double **A, int n) {
   /*PATSMA*/
   /* CSA parameters*/
   int dim = 1;
@@ -105,8 +73,7 @@ void solve_parallel(double **A, int n)
   int n_iter = 20;
 
   /*PATSMA INSTANCE*/
-  Autotuning *at = new Autotuning(min,max,ignore,dim,n_opt,n_iter);
-
+  Autotuning *at = new Autotuning(min, max, ignore, dim, n_opt, n_iter);
 
   int *chunk = new int[dim];
   at->entireExecRuntime(matrix_calculation, chunk, A, N - 1);
@@ -118,16 +85,14 @@ void solve_parallel(double **A, int n)
   double tmp;
   double diff;
   int i, j;
-  for (for_iters = 1; for_iters < 21; ++for_iters)
-  {
+  for (for_iters = 1; for_iters < 21; ++for_iters) {
     diff = 0;
 
     diff = matrix_calculation(A, N - 1, chunk);
     iters++;
 
     printf("Difference after %3d iterations: %f\n", iters, diff);
-    if (diff / ((double)N * (double)N) < Tolerance)
-    {
+    if (diff / ((double)N * (double)N) < Tolerance) {
       printf("\nConvergence achieved after %d iterations....Now exiting\n\n", iters);
       return;
     }
@@ -135,15 +100,13 @@ void solve_parallel(double **A, int n)
   printf("\n\nIteration LIMIT Reached...Exiting\n\n");
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
   int i;
   long t_start, t_end;
   double time;
   A = new double *[N + 2];
-  for (i = 0; i < N + 2; i++)
-  {
+  for (i = 0; i < N + 2; i++) {
     A[i] = new double[N + 2];
   }
 
